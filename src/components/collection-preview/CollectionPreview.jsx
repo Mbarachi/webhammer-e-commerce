b0b2aa12d76a44d73987ef40e47d8b169e0beefe
Mbarachi <39647480+Mbarachi@users.noreply.github.com>
2021-06-07 12:51:50 +0100

    Updatign the shop page with some collection-items

--- src/components/collection-preview/CollectionPreview.jsx
+++ src/components/collection-preview/CollectionPreview.jsx
@@ -1,12 +1,14 @@
+import CollectionItem from '../collection-item/CollectionItem'
 import './collection-preview.styles.scss'
+
 
 const CollectionPreview = ({title, items }) => {
     return (
         <div className='collection-preview'>
             <h1 className='title'>{title.toUpperCase()}</h1>
             <div className='preview'>
-                {items.filter((item,idx) => idx < 4).map(item => (
-                    <div>{item.name}</div>
+                {items.filter((item,idx) => idx < 4).map(({id, ...otherItemProps}) => (
+                    <CollectionItem key={id} {...otherItemProps}/>
                 ))} 
             </div>
         </div>
